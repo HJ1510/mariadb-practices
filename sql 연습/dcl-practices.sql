bc503ff581b5abcc5c5588de5fef2f8f7b81dc40
HJ1510 <yhj221004@gmail.com>
2022-10-11 17:51:02 +0900

    re

--- sql 연습/dcl-practices.sql
+++ sql 연습/dcl-practices.sql
@@ -4,4 +4,14 @@ create database webdb;
 -- 데이터베이스 생성 확인
 show databases;
 
+-- db사용자 삭제
+drop user'webdb'@'locahost';
+
 -- db 사용자 생성
+create user 'webdb'@'localhost' identified by 'webdb';
+
+-- 권한 부여
+grant all privileges on webdb.* to 'webdb'@'locahost';
+
+-- 권한 정보 재로딩
+flush privileges;
