ba2815772e7fd46b4bff793b16b42aa1540014ff
HJ1510 <yhj221004@gmail.com>
2022-11-11 17:55:38 +0900

    mysite02 board

--- jdbc-practices/SQL/guestbook user.sql
+++ jdbc-practices/SQL/guestbook user.sql
@@ -24,4 +24,18 @@ select no from user where no=3;
 update user set name='마이콜', password='1111', gender='male' where no=3;
 update user set name='마이콜', gender='male' where no=3;
 
+desc board;
 
+select * from `user` u ;
+
+select * from board b ;
+
+insert into board values(null, '멍', '...', 0, now(), 1 , 1, 0, (select no from user where name='둘리'));
+
+select a.no, a.title, a.contents, a.hit, date_format(a.reg_date, '%Y/%m/%d %H:%i:%s' ),a.group_no, a.order_no, a.depth , a.user_no, b.name  
+from board a, user b
+where a.user_no =b.no 
+order by group_no desc, order_no asc;
+limit (page-1)*3,3
+
+pstmt.setInt(1,(page-1)*3)
