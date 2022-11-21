ef3fd15d504e75f8091a638c289ae5d4a6d23615
HJ1510 <114970797+HJ1510@users.noreply.github.com>
2022-11-21 10:56:40 +0900

    Update guestbook user.sql

--- jdbc-practices/SQL/guestbook user.sql
+++ jdbc-practices/SQL/guestbook user.sql
@@ -26,20 +26,30 @@ update user set name='마이콜', gender='male' where no=3;
 
 desc board;
 
-select * from `user` u ;
-
 select * from board b ;
 
 insert into board values(null, '안녕2', '헬로우~~', 0, now(), 1 , 1, 0, (select no from user where name='둘리'));
 
 update board set user_no = 3 where title='그만';
 
-select title , contents from board where no=3 ;
+select title , contents, user_no from board where no=3 ;
 
 select a.no, a.title, a.contents, a.hit, date_format(a.reg_date, '%Y/%m/%d %H:%i:%s' ),a.group_no, a.order_no, a.depth , a.user_no, b.name 
 from board a, user b 
 where a.user_no =b.no 
 order by no desc, group_no desc, order_no asc;
-limit (page-1)*3,3
+-- limit (page-1)*3,3
+-- pstmt.setInt(1,(page-1)*3)
 
-pstmt.setInt(1,(page-1)*3)
+update board set hit=hit+1 where no=3;
+
+select no, name, contents, date_format(reg_date, '%Y/%m/%d %H:%i:%s') from guestbook order by reg_date desc;
+
+insert into board values(null, '안녕2', '헬로우~~', 0, now(), max(group_no)+1 , 1, 0, (select no from user where name='둘리'));
+
+select no, name, contents, reg_date from guestbook order by reg_date desc;
+
+select no, name, contents, reg_date as regDate from guestbook order by regDate asc;
+
+insert into guestbook values(null, '둘리', '1234', '호이', now());
+select last_insert_id();
